dc83093066cfdb5cf687c78f21b4d488b45a7266
tiankuncampus <tiankuncampus@163.com>
2016-10-28 13:17:49 +0800

    first commit

---
+++ helloword.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        cout << "hello word!"<< endl;
+        sleep(1);
+    };
+    return 0;
+}
